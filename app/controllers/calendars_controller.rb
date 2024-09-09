e0820cf4bdb2d832aec77922d0e41bb3d62a40a0
akiko-web <ziakohappp@gmail.com>
2024-09-09 16:07:08 +0900

    Issue5

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -17,12 +17,6 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
-  def create
-    binding.pry
-    Plan.create(plan_params)
-    redirect_to action: :index
-  end
-  
   private
 
   def plan_params
